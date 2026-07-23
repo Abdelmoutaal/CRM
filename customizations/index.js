fcbb3624577a1187e9dd44c70336eb5e105b5bc7
abdessamad <166138724+Abdessamadmtallah@users.noreply.github.com>
2026-07-23 17:34:14 +0100

    change colors

--- customizations/index.js
+++ customizations/index.js
@@ -2285,10 +2285,10 @@ var callToActionStyle = {
 	display: "inline-flex",
 	padding: "8px 32px",
 	borderRadius: emailTheme.border.radius.md,
-	border: `1px solid ${emailTheme.background.transparent.light}`,
-	background: emailTheme.background.button,
-	boxShadow: `0px 2px 4px 0px ${emailTheme.background.transparent.light}, 0px 0px 4px 0px ${emailTheme.background.transparent.medium}`,
-	color: emailTheme.font.colors.inverted,
+	border: "1px solid #B8965A",
+	background: "#B8965A",
+	boxShadow: "none",
+	color: "#FCFBF9",
 	fontSize: emailTheme.font.size.md,
 	fontWeight: emailTheme.font.weight.bold,
 	width: "auto"
@@ -2678,11 +2678,11 @@ var HighlightedContainer = ({ children }) => {
 //#region src/components/HighlightedText.tsx
 var highlightedStyle = {
 	borderRadius: emailTheme.border.radius.sm,
-	background: emailTheme.background.colors.highlight,
+	background: "transparent",
 	padding: "4px 8px",
 	fontSize: emailTheme.font.size.lg,
 	fontWeight: emailTheme.font.weight.bold,
-	color: emailTheme.font.colors.highlighted
+	color: "#2D2A26"
 };
 var HighlightedText = ({ value }) => {
 	return /*#__PURE__*/ (0, react_jsx_runtime.jsx)(Text, {
