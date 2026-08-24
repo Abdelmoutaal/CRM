a6e0f45d9076134e65e49f1e67070fa5105b9ebd
omarichou <omarichou567@gmail.com>
2026-08-24 22:19:24 +0100

    up

--- customizations/rls-demote-tt.sql
+++
@@ -1,7 +0,0 @@
--- Rétrograder le rôle tt en rôle non-admin
-UPDATE core."role"
-SET "canUpdateAllSettings" = false,
-    "updatedAt" = now()
-WHERE label = 'tt' AND "canUpdateAllSettings" = true;
-
-SELECT label, "canUpdateAllSettings" FROM core."role" ORDER BY label;
